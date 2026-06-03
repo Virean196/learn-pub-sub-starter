32ff374aca8f3659fd11b17da88e7e4f24edf97d
Virean <gama.bruno.196@gmail.com>
2026-06-03 07:14:48 +0200

    CH3:L6 - Done

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Unable to connect to RabbitMQ: %s", err)
+	}
+	defer connection.Close()
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Print(err)
+	}
+	pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.SimpleQueueTransient)
+	// Wait for os.Interrupt
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	<-signalCh
 }
--- internal/pubsub/publishing.go
+++ internal/pubsub/publishing.go
@@ -7,6 +7,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	marshalledVal, err := json.Marshal(val)
 	if err != nil {
@@ -14,4 +21,16 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	}
 	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/json", Body: marshalledVal})
 	return nil
+}
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+	connCh, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	queue, err := connCh.QueueDeclare(queueName, queueType == SimpleQueueDurable, queueType == SimpleQueueTransient, queueType == SimpleQueueTransient, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	return connCh, queue, nil
 }
